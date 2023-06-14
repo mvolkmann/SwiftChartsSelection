397f8b6b5981b19487f79323796d029b1b808732
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-14 09:29:30 -0500

    Initial Commit

---
+++ SwiftChartsSelection/SwiftChartsSelectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsSelectionApp.swift
+//  SwiftChartsSelection
+//
+//  Created by Mark Volkmann on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsSelectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
